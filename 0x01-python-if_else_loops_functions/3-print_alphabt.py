b580b41c25b4b6d6e6fd8223c876b7b42eba2566
mdthr_ah <modather11.a@gmail.com>
2023-03-08 23:33:44 +0200

    edit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
+# Author - mdthr
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
